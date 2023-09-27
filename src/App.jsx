9b0c7d378b00e1b5ac3a460fef24703411e061a5
Atamyrat <guwancikramow@gmail.com>
2023-09-27 16:43:48 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './pages/home'
+import Home from './pages/Home'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import NotFound from './pages/NotFound'
 
